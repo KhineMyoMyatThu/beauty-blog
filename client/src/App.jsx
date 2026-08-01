9140d0746612bef14c5c469de79bcb478b6ff7b6
KhineMyoMyatThu <khinemyomyatthu24@gmail.com>
2026-08-01 00:15:01 -0700

    Adding library for routing

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home</div>
+  },
+]);
+
+
+
+function App() {
+  <div className="">
+    <RouteProvider router={route}/>
+  </div>
+}
+
+export default App;
